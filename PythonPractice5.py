81451a5c505a54ab40ccb7014d5fd978610f62a3
Ourplanet <mig070@naver.com>
2019-07-23 11:55:22 +0900

    Add files via upload

---
+++ PythonPractice5.py
@@ -0,0 +1,74 @@
+# l = list()
+l = []
+
+
+#append (추가 함수)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+l.append(5)
+
+
+# insert (i라는 인덱스에 x값 추가)
+# list.insert(i,x)
+
+# l.insert(1, 3)
+
+
+# remove (x라는 값과 동일한 첫번쨰 항목을 삭제함)
+# list.remove(x)
+
+# l.remove(3)
+
+
+# pop (i라는 인덱스의 값을 변환하면서, 삭제합니다.)
+# list.pop(i)
+
+# n = l.pop(1)
+# print(n, l)
+
+
+# clear (리스트의 모든 항목을 삭제합니다)
+# list.clear()
+
+# l.clear()
+# print(l)
+
+
+# index (x와 같은 첫번째 것의 0부터 시작하는 인덱스를 반환합니다.
+# 만약, x가 존재하지 않는다면 ValueError를 발생시킵니다.)
+# list.index(x[, start[, end]])
+
+# print(l.index(5))
+# print(6 in l) #False
+
+
+# count (x가 리스트 안에서 몇 개가 있는지 구해주는 함수
+# list.count(x)
+
+# print(l.count(3))
+
+
+# sort (리스트를 오름차순으로 정렬)
+# list.sort()
+
+# l.sort()
+# print(l)
+
+
+# 내림차순 sort
+# list.sort(reverse=True)
+
+# l.sort(reverse=True)
+# print(l)
+
+
+# reverse (리스트를 꺼꾸로 뒤집어줍니다)
+# l.reverse()
+# print(l)
+
+
+# 리스트에 있는 요소를 앞에서부터 하나하나 가져옴
+# for n in l :
+#     print(n)
